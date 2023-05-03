e0a4844ea47e84327f133c1bf9818ed0360b5835
mbright90 <michael.bright90@icloud.com>
2023-05-03 10:26:45 +0100

    Fix linting errors

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
-import React from "react";
-import ReactDOM from 'react-dom'
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React from 'react'
+import { render } from 'react-dom'
 import App from './components/app'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+render(<App />, document.getElementById('root'))
