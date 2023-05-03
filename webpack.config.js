1eb3850ef2a3e08d22cd222b5babe8caa4820602
mbright90 <michael.bright90@icloud.com>
2023-05-03 10:26:17 +0100

    Update existing configs for ESLint

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,28 @@ const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: "./src/index.tsx",
-  output: {
-    path: path.join(__dirname, './build'),
-    filename: 'main.js'
-},
-  plugins: [
-    new HTMLWebpackPlugin({
-        template: '.src/index.html'
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-    ],
-  },
-};
+	entry: './src/index.tsx',
+	output: {
+		path: path.join(__dirname, './build'),
+		filename: 'main.js',
+	},
+	plugins: [
+		new HTMLWebpackPlugin({
+			template: './public/index.html',
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+		],
+	},
+	devServer: {
+		contentBase: path.join(__dirname, 'dist'),
+		compress: true,
+		port: 9000,
+	},
+}
