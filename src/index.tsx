efc1b5b2f90ebd8ad6b245920cd6996a71ebe59a
mbright90 <michael.bright90@icloud.com>
2023-05-05 10:24:39 +0100

    Update root creation

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import React from 'react'
-import { render } from 'react-dom'
-import App from './components/app'
+import { createRoot } from 'react-dom/client'
+import App from '@components/app'
 
-render(<App />, document.getElementById('root'))
+const root = createRoot(document.getElementById('root')!)
+
+root.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+)
