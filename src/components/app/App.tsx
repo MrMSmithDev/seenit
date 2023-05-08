5209aee68fe7548e121aae05a236ed5a3be15e26
mbright90 <michael.bright90@icloud.com>
2023-05-08 17:35:02 +0100

    Linting fixes

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,7 +1,15 @@
+import Header from '@components/header'
 import React from 'react'
 
+import styles from './App.module.scss'
+
 const App: React.FC = () => {
-	return <div>Hello world</div>
+  return (
+    <div className={styles.appWrapper}>
+      <Header />
+      <p>Hello world</p>
+    </div>
+  )
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import '@styles/styles.scss'
 const root = createRoot(document.getElementById('root')!)
 
 root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
