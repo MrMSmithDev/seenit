d3456c92cd9f405dd723221da75e431d557b0943
mbright90 <michael.bright90@icloud.com>
2023-05-08 17:54:19 +0100

    Implement Header component

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import style from './Header.module.scss'
+
+const Header: React.FC = () => {
+  return <div className={style.header}>Header</div>
+}
+
+export default Header
