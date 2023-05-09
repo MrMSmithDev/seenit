cfe5b1f2b0616297907f2c54be39655c967c2714
mbright90 <michael.bright90@icloud.com>
2023-05-09 12:40:42 +0100

    Add logo image to header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { logo } from '@assets/images'
 
 import style from './Header.module.scss'
 
 const Header: React.FC = () => {
-  return <div className={style.header}>Header</div>
+  return (
+    <div className={style.header}>
+      <img src={logo} alt="SeenIt-logo-blue"></img>
+    </div>
+  )
 }
 
 export default Header
