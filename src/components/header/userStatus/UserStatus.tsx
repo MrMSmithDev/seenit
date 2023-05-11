cb18b3af487e72b1de2817a2818b223d4aba669b
mbright90 <michael.bright90@icloud.com>
2023-05-11 11:40:17 +0100

    Add UserStatus component skeleton

---
+++ src/components/header/userStatus/UserStatus.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import style from './UserStatus.module.scss'
+
+const UserStatus: React.FC = () => {
+  return <div className={style.userStatus}></div>
+}
+
+export default UserStatus
