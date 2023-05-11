8ea1c795d50153e1ba72fddf56fd6eb6cc49e1ec
mbright90 <michael.bright90@icloud.com>
2023-05-11 11:35:42 +0100

    Add UserStatus component and positioning

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { logo } from '@assets/images'
+import UserStatus from './userStatus'
 
 import style from './Header.module.scss'
 
 const Header: React.FC = () => {
+  //Create useState here which adjusts the size of the header when scrolled down.
+
   return (
     <div className={style.header}>
-      <img src={logo} alt="SeenIt-logo-blue"></img>
+      <img className={style.headerLogo} src={logo} alt="SeenIt-logo-blue"></img>
+      <UserStatus />
     </div>
   )
 }
