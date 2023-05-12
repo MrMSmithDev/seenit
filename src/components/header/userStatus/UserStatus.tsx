3f977c1f7bfc4724902bf3ff0b758706cdf4a287
mbright90 <michael.bright90@icloud.com>
2023-05-12 13:15:01 +0100

    Add basic functionality to userStatus

--- src/components/header/userStatus/UserStatus.tsx
+++ src/components/header/userStatus/UserStatus.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
 
 import style from './UserStatus.module.scss'
 
 const UserStatus: React.FC = () => {
-  return <div className={style.userStatus}></div>
+  return (
+    <div className={`${style.userStatus} ${style.notLoggedIn}`}>
+      <div className={style.userImgWrapper}>
+        <FontAwesomeIcon className={style.userIcon} icon={faUser} />
+      </div>
+      <p>Login</p>
+    </div>
+  )
 }
 
 export default UserStatus
