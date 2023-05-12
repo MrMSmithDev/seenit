adc0e84582399cb381bf5f95f1734b4cc2523e56
mbright90 <michael.bright90@icloud.com>
2023-05-12 13:12:29 +0100

    Initiate custom hooks directory

---
+++ src/hooks/usePostFeed/usePostFeed.ts
@@ -0,0 +1,11 @@
+import { useEffect, useState } from 'react'
+import { PostCollection } from 'src/customTypes/types'
+
+type UsePostFeedResult = [PostCollection, boolean]
+
+// function usePostFeed(): UsePostFeedResult {
+// }
+
+function usePostFeed() {}
+
+export default usePostFeed
