9a36bfe7564820d15965751010b673431308fd1a
mbright90 <michael.bright90@icloud.com>
2023-05-15 11:32:03 +0100

    Refine PostType custom type

--- src/customTypes/types.ts
+++ src/customTypes/types.ts
@@ -1,5 +1,9 @@
-export interface Post {
+export interface PostType {
   id: string
+  dateTime: string
+  author: string
+  title: string
+  body: string
 }
 
-export type PostCollection = Array<Post>
+export type PostCollection = Array<PostType>
