23361db4faffe116921b1de28c187d5969abb1f6
mbright90 <michael.bright90@icloud.com>
2023-05-16 14:15:07 +0100

    Add Comment custom typing

--- src/customTypes/types.ts
+++ src/customTypes/types.ts
@@ -5,6 +5,14 @@ export interface PostType {
   title: string
   body: string
   edited?: boolean
+  comments?: Array<Comment>
+}
+
+export interface Comment {
+  id: string
+  timeStamp: string
+  author: string
+  body: string
 }
 
 export type PostCollection = Array<PostType>
