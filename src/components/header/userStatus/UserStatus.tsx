c7a0a0fe9ac3c65d64d4e1572f7ea7f8851003a8
mbright90 <michael.bright90@icloud.com>
2023-05-16 14:14:18 +0100

    Update userStatus with drop menu

--- src/components/header/userStatus/UserStatus.tsx
+++ src/components/header/userStatus/UserStatus.tsx
@@ -1,16 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser } from '@fortawesome/free-solid-svg-icons'
 
 import style from './UserStatus.module.scss'
 
 const UserStatus: React.FC = () => {
+  const [dropIsActive, setDropIsActive] = useState(false)
+  const [authStatus, setAuthStatus] = useState(false)
+
+  function signOut(event: React.MouseEvent<HTMLButtonElement>) {
+    event.stopPropagation()
+    setAuthStatus(!authStatus)
+  }
+
+  function showDropDown() {
+    setDropIsActive(!dropIsActive)
+  }
+
   return (
-    <div className={`${style.userStatus} ${style.notLoggedIn}`}>
+    <div className={`${style.userStatus} ${style.notLoggedIn}`} onClick={showDropDown}>
       <div className={style.userImgWrapper}>
         <FontAwesomeIcon className={style.userIcon} icon={faUser} />
       </div>
       <p>Login</p>
+      <div className={`${style.dropDownMenu} ${dropIsActive ? style.isActive : ''}`}>
+        <button className={style.signOutButton} onClick={signOut}>
+          Sign Out
+        </button>
+      </div>
     </div>
   )
 }
