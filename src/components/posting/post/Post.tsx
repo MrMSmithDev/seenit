84147274a95cbe3312e2f7fef509ad73f54d1ecd
mbright90 <michael.bright90@icloud.com>
2023-05-16 14:14:45 +0100

    Add comments visbility to posts

--- src/components/posting/post/Post.tsx
+++ src/components/posting/post/Post.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 import style from './Post.module.scss'
 import { PostType } from 'src/customTypes/types'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar as solidStar } from '@fortawesome/free-solid-svg-icons'
+import { faStar as hollowStar, faMessage } from '@fortawesome/free-regular-svg-icons'
 
 interface PostProps {
   currentPost: PostType
@@ -9,14 +12,24 @@ interface PostProps {
 }
 
 const Post: React.FC<PostProps> = ({ currentPost, isPreview }) => {
+  const userHasFavorite = false
   const postStyle = isPreview ? `${style.post} ${style.postPreview}` : style.postBody
 
   return (
     <div className={postStyle}>
-      <p className={style.postTitle}>{currentPost.title}</p>
+      <p className={style.headline}>
+        <FontAwesomeIcon
+          className={style.starIcon}
+          icon={userHasFavorite ? solidStar : hollowStar}
+        />
+        <span className={style.postTitle}>{currentPost.title}</span>
+      </p>
       <p className={style.postBody}>{currentPost.body}</p>
       <div className={style.postInfo}>
         <p className={style.postTimestamp}>{currentPost.timeStamp}</p>
+        <p className={style.postCommentCount}>
+          {currentPost.comments?.length || 0} <FontAwesomeIcon icon={faMessage} />
+        </p>
         <p className={style.postAuthor}>{currentPost.author}</p>
       </div>
     </div>
