a6db8f771e70d3199c4b3cb89f92afa3e597689a
mbright90 <michael.bright90@icloud.com>
2023-05-17 13:57:03 +0100

    Initiate and implement useAuth hook

---
+++ src/hooks/useAuth/useAuth.ts
@@ -0,0 +1,53 @@
+import { useEffect, useState } from 'react'
+import {
+  getAuth,
+  onAuthStateChanged,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+  User as FirebaseUser
+} from 'firebase/auth'
+
+export function useAuth() {
+  const [user, setUser] = useState<FirebaseUser | null>(null)
+
+  useEffect(() => {
+    const auth = getAuth()
+    const unsubscribe = onAuthStateChanged(auth, (userChange) => {
+      setUser(userChange)
+    })
+
+    return () => unsubscribe()
+  }, [])
+
+  async function signIn(): Promise<void> {
+    const provider = new GoogleAuthProvider()
+    await signInWithPopup(getAuth(), provider)
+  }
+
+  async function signOutUser(): Promise<void> {
+    await signOut(getAuth())
+  }
+
+  function getUserName(): string | null {
+    return user?.displayName || null
+  }
+
+  function getUserPicture(): string | null {
+    return user?.photoURL || null
+  }
+
+  function isUserSignedIn(): boolean {
+    return !!user
+  }
+
+  return {
+    user,
+    signIn,
+    signOutUser,
+
+    getUserName,
+    getUserPicture,
+    isUserSignedIn
+  }
+}
