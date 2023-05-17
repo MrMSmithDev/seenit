e7bdcb2d785bfebc646f888633d776a8ff9477d4
mbright90 <michael.bright90@icloud.com>
2023-05-17 13:53:22 +0100

    Initialize firebase connection on opening

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,17 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from '@components/app'
 
+import { initializeApp } from 'firebase/app'
+import { firebaseConfig } from './firebaseConfig'
+
 import '@styles/styles.scss'
+
+try {
+  initializeApp(firebaseConfig)
+} catch (error) {
+  // eslint-disable-next-line no-console
+  console.error('Could not initialize Firestore', error)
+}
 
 const root = createRoot(document.getElementById('root')!)
 
