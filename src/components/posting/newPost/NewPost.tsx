a101007380ff051e7223d27843046234ad193521
mbright90 <michael.bright90@icloud.com>
2023-05-18 15:43:46 +0100

    Initiate newPost form component

---
+++ src/components/posting/newPost/NewPost.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import style from './NewPost.module.scss'
+
+const NewPost: React.FC = () => {
+  return (
+    <div className={style.newPostContainer}>
+      <h1>New Post</h1>
+      <form>
+        <div className={style.inputContainer}>
+          <label htmlFor="post-title">Post Title</label>
+          <input
+            type="text"
+            id="post-title"
+            name="post-title"
+            minLength={3}
+            maxLength={30}
+            required
+          />
+        </div>
+        <div className={style.inputContainer}>
+          <label htmlFor="post-image">Add Image</label>
+          <input type="file" />
+        </div>
+        <div className={style.inputContainer}>
+          <label htmlFor="">Post Body</label>
+          <textarea id="post-body" name="post-body" minLength={10} />
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default NewPost
