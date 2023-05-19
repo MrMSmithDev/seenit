e63b8ee94f02547ac3e6bc2b8d245d7b43f5b54a
mbright90 <michael.bright90@icloud.com>
2023-05-19 10:37:44 +0100

    Import and implement newPostForm for styling

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,12 +1,13 @@
 import Header from '@components/header'
 import LandingPage from '@components/landingPage'
 import Main from '@components/main'
-import PostFeed from '@components/posting/postFeed'
+// import PostFeed from '@components/posting/postFeed'
 import UserBar from '@components/userBar'
 import React from 'react'
 import { useAuth } from '@hooks/useAuth'
 
 import styles from './App.module.scss'
+import NewPostForm from '@components/posting/newPostForm'
 
 const App: React.FC = () => {
   const { isUserSignedIn } = useAuth()
@@ -18,7 +19,8 @@ const App: React.FC = () => {
       <Header />
       <Main>
         <UserBar />
-        <PostFeed />
+        <NewPostForm />
+        {/* <PostFeed /> */}
       </Main>
     </div>
   )
