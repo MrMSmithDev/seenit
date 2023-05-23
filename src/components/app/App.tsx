f1721ae5622e53010cffa694689d1204e3e9e527
mbright90 <michael.bright90@icloud.com>
2023-05-23 12:18:54 +0100

    Alter useAuth import

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -4,10 +4,10 @@ import Main from '@components/main'
 import PostFeed from '@components/posting/postFeed'
 import UserBar from '@components/userBar'
 import React from 'react'
-import { useAuth } from '@hooks/useAuth'
+import useAuth from '@hooks/useAuth'
 
 import styles from './App.module.scss'
-import NewPostForm from '@components/posting/newPostForm'
+// import NewPostForm from '@components/posting/newPostForm'
 
 const App: React.FC = () => {
   const { isUserSignedIn } = useAuth()
