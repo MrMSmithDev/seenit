e55ddb98de1040a2417c8bdcda86c88394811ff2
mbright90 <michael.bright90@icloud.com>
2023-05-24 10:41:24 +0100

    Add initial logic to use loading screen

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -3,14 +3,27 @@ import LandingPage from '@components/landingPage'
 import Main from '@components/main'
 import PostFeed from '@components/posting/postFeed'
 import UserBar from '@components/userBar'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import useAuth from '@hooks/useAuth'
 
 import styles from './App.module.scss'
+import { logo } from '@assets/images'
 // import NewPostForm from '@components/posting/newPostForm'
 
 const App: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(true)
   const { isUserSignedIn } = useAuth()
+
+  useEffect(() => {
+    setIsLoading(!isLoading)
+  }, [])
+
+  if (isLoading)
+    return (
+      <div className={styles.loading}>
+        <img src={logo} alt="Seenit logo" />
+      </div>
+    )
 
   if (!isUserSignedIn()) return <LandingPage />
 
