547907f4764b5a377e51ad4ecf7c6151060d4705
mbright90 <michael.bright90@icloud.com>
2023-05-26 10:32:39 +0100

    Convert buttons to router Links

--- src/components/userBar/UserBar.tsx
+++ src/components/userBar/UserBar.tsx
@@ -1,17 +1,37 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import style from './UserBar.module.scss'
 
 const UserBar: React.FC = () => {
   return (
     <div className={style.userBar}>
-      <button>New Post</button>
-      <button>My Posts</button>
-      <button>My Comments</button>
-      <button>My Favorites</button>
-      <button>Settings</button>
+      <Link to="/" className={style.sidebarLink} tabIndex={1} data-testid="sidebarLink">
+        Homepage
+      </Link>
+      <Link to="/posts/new" className={style.sidebarLink} tabIndex={2} data-testid="sidebarLink">
+        New Post
+      </Link>
+      <Link to="/" className={style.sidebarLink} tabIndex={3} data-testid="sidebarLink">
+        My Posts
+      </Link>
+      <Link to="/" className={style.sidebarLink} tabIndex={4} data-testid="sidebarLink">
+        My Comments
+      </Link>
+      <Link to="/" className={style.sidebarLink} tabIndex={5} data-testid="sidebarLink">
+        My Favorites
+      </Link>
+      <Link to="/" className={style.sidebarLink} tabIndex={6} data-testid="sidebarLink">
+        Settings
+      </Link>
     </div>
   )
 }
 
 export default UserBar
+
+// to={`/products/${product.id}`}
+// className={style.productCard}
+// data-product-id={product.id}
+// tabIndex="-1"
+// data-testid="product-card"
