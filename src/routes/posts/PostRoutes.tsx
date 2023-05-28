2fa5f7e67e469a6262e11416f5fb6b32fabc50d7
mbright90 <michael.bright90@icloud.com>
2023-05-28 11:19:35 +0100

    Alter Post component route

--- src/routes/posts/PostRoutes.tsx
+++ src/routes/posts/PostRoutes.tsx
@@ -1,19 +1,17 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 
-import Post from '@components/posting/post'
+// import Post from '@components/posting/post'
 import NewPostForm from '@components/posting/newPostForm'
 import PostFeed from '@components/posting/postFeed'
+import Post from '@components/posting/post'
 
 const PostRoutes: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<PostFeed />} />
       <Route path="/posts/new" element={<NewPostForm />} />
-      <Route
-        path="/posts/:postID"
-        element={<Post currentPost={{ ID: '', authorID: '', body: '', title: '' }} />}
-      />
+      <Route path="/posts/:postID/:postTitle" element={<Post />} />
     </Routes>
   )
 }
