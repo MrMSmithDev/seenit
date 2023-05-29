fad90608cf4bdc8fca2b4e21637fcfa6e16f03dc
mbright90 <michael.bright90@icloud.com>
2023-05-29 17:05:40 +0100

    Add success message when comments added

--- src/components/comments/newCommentForm/NewCommentForm.tsx
+++ src/components/comments/newCommentForm/NewCommentForm.tsx
@@ -1,27 +1,61 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+
+import useComments from '@hooks/useComments'
 
 import style from './NewCommentForm.module.scss'
+import { useParams } from 'react-router-dom'
 
 const NewCommentForm: React.FC = () => {
+  const { postID } = useParams()
   const [commentBody, setCommentBody] = useState('')
+  const [commentSuccess, setCommentSuccess] = useState(false)
+  const { writeComment } = useComments()
 
-  const uploadComment = () => {
-    console.log(commentBody)
+  useEffect(() => {
+    let timeoutID
+    if (commentSuccess)
+      timeoutID = setTimeout(() => {
+        setCommentSuccess(false)
+      }, 3000)
+
+    return clearTimeout(timeoutID)
+  }, [commentSuccess])
+
+  const uploadComment = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    const resultBool = await writeComment(postID!, commentBody)
+    if (resultBool) {
+      setCommentBody('')
+      setCommentSuccess(true)
+    }
   }
 
   const handleCommentBodyChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setCommentBody(e.target.value)
   }
 
+  const successMessage = (
+    <div>
+      <p>Comment added</p>
+    </div>
+  )
+
   return (
     <div className={style.newCommentContainer}>
       <form className={style.newCommentForm}>
-        <label>Comment</label>
-        <textarea onChange={handleCommentBodyChange} />
+        <label htmlFor="comment-input">Add Comment</label>
+        <textarea
+          id="comment-input"
+          name="comment-input"
+          minLength={3}
+          maxLength={400}
+          onChange={handleCommentBodyChange}
+        />
         <button type="submit" onClick={uploadComment}>
           Confirm
         </button>
       </form>
+      {commentSuccess ? successMessage : null}
     </div>
   )
 }
