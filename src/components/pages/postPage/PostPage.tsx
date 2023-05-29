550b42adb14c7af28566ebd10252a3317fe7f5f8
mbright90 <michael.bright90@icloud.com>
2023-05-29 17:06:53 +0100

    Render newCommentForm and commentFeed

--- src/components/pages/postPage/PostPage.tsx
+++ src/components/pages/postPage/PostPage.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import Post from '@components/posting/post'
 
 import style from './PostPage.module.scss'
+import NewCommentForm from '@components/comments/newCommentForm'
+import CommentFeed from '@components/comments/commentFeed'
 
 const PostPage: React.FC = () => {
   return (
     <div className={style.postPage}>
       <Post />
-      <div>Comment space</div>
+      <NewCommentForm />
+      <CommentFeed />
     </div>
   )
 }
