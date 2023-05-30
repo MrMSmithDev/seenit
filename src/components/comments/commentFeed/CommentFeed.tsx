72f0d683f2a40e491013ada3bcf4f1bbef91cc47
mbright90 <michael.bright90@icloud.com>
2023-05-30 12:32:25 +0100

    Load comments and map into Node array

--- src/components/comments/commentFeed/CommentFeed.tsx
+++ src/components/comments/commentFeed/CommentFeed.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { CommentType } from 'src/customTypes/types'
+import useComments from '@hooks/useComments'
 
 import style from './CommentFeed.module.scss'
+import { useParams } from 'react-router-dom'
+import Comment from '../comment'
 
 const CommentFeed: React.FC = () => {
-  return <div className={style.commentFeed}></div>
+  const [currentComments, setCurrentComments] = useState<CommentType[]>([])
+  const { postID } = useParams()
+  const { loadCommentFeed } = useComments()
+
+  useEffect(() => {
+    const loadComments = async () => {
+      const commentFeed = await loadCommentFeed(postID!)
+      setCurrentComments(commentFeed)
+    }
+
+    loadComments()
+  }, [])
+
+  const commentArr = currentComments.map((comment: CommentType) => {
+    return <Comment comment={comment} key={comment.ID} />
+  })
+
+  return <div className={style.commentFeed}>{commentArr}</div>
 }
 
 export default CommentFeed
