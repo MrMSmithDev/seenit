1a9943df0d5e9387e4175e209ec9f57574cfe460
mbright90 <michael.bright90@icloud.com>
2023-05-30 11:26:48 +0100

    Add function to add commentID to post

--- src/hooks/useComments/useComments.ts
+++ src/hooks/useComments/useComments.ts
@@ -2,7 +2,17 @@
 //*******************//
 //***** Comments ****//
 //*******************//
-import { addDoc, collection, getFirestore, serverTimestamp } from 'firebase/firestore'
+import {
+  addDoc,
+  arrayUnion,
+  collection,
+  getDocs,
+  getFirestore,
+  query,
+  serverTimestamp,
+  updateDoc,
+  where
+} from 'firebase/firestore'
 import useAuth from '@hooks/useAuth'
 
 // function loadComments
@@ -12,14 +22,31 @@ import useAuth from '@hooks/useAuth'
 const generateCommentID = (): string => {
   const commentID: number = Math.floor(Math.random()) * Date.now()
   const formattedID: string = commentID.toLocaleString(undefined, { maximumSignificantDigits: 9 })
+  console.log(formattedID)
   return formattedID
 }
+
+async function addCommentToPost(postID: string, commentID: string): Promise<void> {
+  try {
+    const postDB = collection(getFirestore(), 'posts')
+    const querySnapshot = await getDocs(query(postDB, where('ID', '==', postID)))
+
+    const postRef = querySnapshot.docs[0].ref
+    await updateDoc(postRef, {
+      comments: arrayUnion(commentID)
+    })
+  } catch (error) {
+    console.error('Error adding comment to post:', error)
+    throw error
+  }
+}
+
 
 function useComments() {
   const firestoreDB = getFirestore()
   const { user } = useAuth()
 
-  async function writeComment(postID: string, commentBody: string) {
+  async function writeComment(postID: string, commentBody: string): Promise<boolean> {
     const commentID = generateCommentID()
     console.log(commentID)
 
@@ -31,21 +58,13 @@ function useComments() {
         author: user?.uid,
         body: commentBody
       })
+      addCommentToPost(postID, commentID)
+      return true
     } catch (error) {
       console.error('Error writing comment:', error)
       throw error
     }
   }
-
-  // function addCommentToPost(postID, commentID) {
-  //     try {
-
-  //     } catch (error) {
-  //         console.error('Error adding comment to post:', error)
-  //         throw error
-  //     }
-  // }
-
   // function loadCommentFeed(postID)
   return {
     writeComment
