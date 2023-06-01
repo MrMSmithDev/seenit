0ee2e01c9e6bdd2120ba9e1704af7a14c856a9e6
mbright90 <michael.bright90@icloud.com>
2023-06-01 10:01:13 +0100

    Add styling to logic to load author

--- src/components/comments/comment/Comment.tsx
+++ src/components/comments/comment/Comment.tsx
@@ -1,5 +1,8 @@
-import React from 'react'
-import { CommentType } from 'src/customTypes/types'
+import React, { useEffect, useState } from 'react'
+import { CommentType, UserType } from 'src/customTypes/types'
+import useUsers from '@hooks/useUsers'
+
+import months from '@utils/months'
 
 import style from './Comment.module.scss'
 
@@ -8,9 +11,34 @@ interface CommentProps {
 }
 
 const Comment: React.FC<CommentProps> = ({ comment }) => {
+  const [author, setAuthor] = useState<UserType>({ uid: '', displayName: '', photoURL: '' })
+  const { loadUserProfile } = useUsers()
+  const timePosted = comment.timeStamp.toDate()
+
+  useEffect(() => {
+    const loadAuthor = async () => {
+      const userProfile = await loadUserProfile(comment.authorID)
+      setAuthor(userProfile)
+    }
+
+    loadAuthor()
+  }, [])
+
   return (
     <div className={style.comment}>
       <p>{comment.body}</p>
+      <div className={style.commentInfo}>
+        <p className={style.commentTimestamp}>
+          {[
+            `${timePosted.getHours()}:${timePosted.getMinutes()} `,
+            `${months[timePosted.getMonth()]} ${timePosted.getFullYear()}`
+          ]}
+        </p>
+        <div className={style.authorContainer}>
+          <img className={style.postAuthorImg} src={author.photoURL} alt="User's image" />
+          <p className={style.postAuthor}>{author.displayName}</p>
+        </div>
+      </div>
     </div>
   )
 }
