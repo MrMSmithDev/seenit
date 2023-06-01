235878b665d95864fd63cc708e69f066ebb02cc5
mbright90 <michael.bright90@icloud.com>
2023-06-01 09:55:42 +0100

    Add utils path to ts alias

--- webpack.config.js
+++ webpack.config.js
@@ -48,7 +48,8 @@ module.exports = {
 			'@components': path.resolve(__dirname, 'src/components'),
 			'@hooks': path.resolve(__dirname, 'src/hooks'),
 			'@routes': path.resolve(__dirname, 'src/routes'),
-			'@styles': path.resolve(__dirname, 'src/styles')
+			'@styles': path.resolve(__dirname, 'src/styles'),
+			'@utils': path.resolve(__dirname, 'src/utils')
 		},
 	},
 }
