e74f56886e130e64d71bcc5d0d3274a19831b50e
mbright90 <michael.bright90@icloud.com>
2023-06-02 09:42:14 +0100

    Initiate and implement voteContainer

---
+++ src/components/comments/comment/voteContainer/VoteContainer.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUpLong, faDownLong, faCircle } from '@fortawesome/free-solid-svg-icons'
+
+import style from './VoteContainer.module.scss'
+
+const VoteContainer: React.FC = () => {
+  const [commentUserPoint, setCommentUserPoint] = useState<number>(0)
+
+  const incrementCommentPoint = () => {
+    if (commentUserPoint != 1) setCommentUserPoint(1)
+    else setCommentUserPoint(0)
+  }
+
+  const decrementCommentPoint = () => {
+    if (commentUserPoint != -1) setCommentUserPoint((prev) => prev - 1)
+    else setCommentUserPoint(0)
+  }
+
+  return (
+    <div className={style.voteContainer}>
+      <button
+        className={style.iconButton}
+        onClick={incrementCommentPoint}
+        style={commentUserPoint === 1 ? { color: 'blue' } : {}}
+      >
+        <FontAwesomeIcon icon={faUpLong} />
+      </button>
+      <div className={style.faIcon}>
+        <FontAwesomeIcon icon={faCircle} />
+      </div>
+      <button
+        className={style.iconButton}
+        onClick={decrementCommentPoint}
+        style={commentUserPoint === -1 ? { color: 'red' } : {}}
+      >
+        <FontAwesomeIcon icon={faDownLong} />
+      </button>
+    </div>
+  )
+}
+
+export default VoteContainer
