40f596845649d1eb63a9bab02a7dccb5666a47f7
mbright90 <michael.bright90@icloud.com>
2023-06-05 17:32:31 +0100

    Create filter table for posts

---
+++ src/utils/filters.js
@@ -0,0 +1,12 @@
+// interface Filter {
+//   text: string
+//   id: string
+// }
+
+const filters = [
+  { text: 'Newest', id: 'new' },
+  { text: 'Oldest', id: 'oldest' },
+  { text: 'Top Rated', id: 'top' }
+]
+
+export default filters
