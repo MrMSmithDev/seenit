a98f1c5346e895a8e67e0b3bd012a87498da81ab
mbright90 <michael.bright90@icloud.com>
2023-06-05 17:33:15 +0100

    Create and implement FilterItems

---
+++ src/components/posting/postFilterBar/postFilterItem/PostFilterItem.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import style from './PostFilterItem.module.scss'
+import { usePosts } from '@hooks/index'
+
+interface PostFilterItemProps {
+  filterText: string
+  filterID: string
+}
+
+const PostFilterItem: React.FC<PostFilterItemProps> = ({ filterText, filterID }) => {
+  const { setFilter } = usePosts()
+  const handleItemClick = () => {
+    setFilter(filterID)
+  }
+
+  return (
+    <button className={style.dropItem} onClick={handleItemClick}>
+      {filterText}
+    </button>
+  )
+}
+
+export default PostFilterItem
