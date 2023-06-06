7e30b3a2eac10a66de459c127100c9d2115a2153
mbright90 <michael.bright90@icloud.com>
2023-06-06 16:41:45 +0100

    Add routing for 'My Comments'

--- src/routes/posts/PostRoutes.tsx
+++ src/routes/posts/PostRoutes.tsx
@@ -5,6 +5,7 @@ import { Route, Routes } from 'react-router-dom'
 import NewPostForm from '@components/posting/newPostForm'
 import PostFeed from '@components/posting/postFeed'
 import PostPage from '@components/pages/postPage'
+import UserCommentFeed from '@components/comments/userCommentFeed'
 
 const PostRoutes: React.FC = () => {
   return (
@@ -13,6 +14,7 @@ const PostRoutes: React.FC = () => {
       <Route path="/posts/new" element={<NewPostForm />} />
       <Route path="/posts/:postID/:postTitle" element={<PostPage />} />
       <Route path="/users/:userID/posts" element={<PostFeed feedTitle={'Posts'} />} />
+      <Route path="/users/:userID/comments" element={<UserCommentFeed />} />
     </Routes>
   )
 }
