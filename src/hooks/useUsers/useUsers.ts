61aa307d852f7b34d4f12dfd3092a81965040ea1
mbright90 <michael.bright90@icloud.com>
2023-06-06 12:02:40 +0100

    Create function to retrieve usersDisplayName

--- src/hooks/useUsers/useUsers.ts
+++ src/hooks/useUsers/useUsers.ts
@@ -5,9 +5,11 @@ import { User } from 'firebase/auth'
 import { getFirestore, collection, setDoc, doc, getDoc } from 'firebase/firestore'
 
 function useUsers() {
+  const firestoreDB = getFirestore()
+
   async function updateUserProfile(currentUser: User) {
     try {
-      const userRef = doc(collection(getFirestore(), 'users'), currentUser.uid)
+      const userRef = doc(collection(firestoreDB, 'users'), currentUser.uid)
       const userDoc = await getDoc(userRef)
       if (userDoc.exists()) {
         await setDoc(
@@ -34,7 +36,7 @@ function useUsers() {
 
   async function loadUserProfile(uid: string): Promise<UserType> {
     try {
-      const userRef = doc(collection(getFirestore(), 'users'), uid)
+      const userRef = doc(collection(firestoreDB, 'users'), uid)
       const userDoc = await getDoc(userRef)
       return userDoc.data() as UserType
     } catch (error) {
@@ -43,9 +45,23 @@ function useUsers() {
     }
   }
 
+  async function getUsersDisplayName(uid: string): Promise<string> {
+    try {
+      const userRef = doc(collection(firestoreDB, 'users'), uid)
+      const userDoc = await getDoc(userRef)
+      const userData = userDoc.data() as UserType
+      return userData.displayName
+    } catch (error) {
+      console.error('Error loading users display name:', error)
+      throw error
+    }
+  }
+
   return {
     updateUserProfile,
-    loadUserProfile
+
+    loadUserProfile,
+    getUsersDisplayName
   }
 }
 
