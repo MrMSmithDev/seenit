3a07d234a4f6c5bf048e721f12284bc38ff5f847
mbright90 <michael.bright90@icloud.com>
2023-06-11 10:15:24 +0100

    Add key iterator for routes

--- src/routes/posts/PostRoutes.tsx
+++ src/routes/posts/PostRoutes.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 
 import NewPostForm from '@components/posting/newPostForm'
 import PostFeed from '@components/posting/postFeed'
@@ -7,14 +7,28 @@ import PostPage from '@components/pages/postPage'
 import UserCommentFeed from '@components/comments/userCommentFeed'
 
 const PostRoutes: React.FC = () => {
+  const location = useLocation()
   return (
     <Routes>
-      <Route path="/" element={<PostFeed feedTitle="Home" />} />
-      <Route path="/posts/new" element={<NewPostForm />} />
-      <Route path="/posts/:postID/:postTitle" element={<PostPage />} />
-      <Route path="/users/:userID/posts" element={<PostFeed feedTitle="Posts" />} />
-      <Route path="/users/:userID/comments" element={<UserCommentFeed />} />
+      <Route key={location.pathname} path="/" element={<PostFeed feedTitle="Home" />} />
+      <Route key={`${location.pathname}-new-post`} path="/posts/new" element={<NewPostForm />} />
       <Route
+        key={`${location.pathname}-post`}
+        path="/posts/:postID/:postTitle"
+        element={<PostPage />}
+      />
+      <Route
+        key={`${location.pathname}-user-posts`}
+        path="/users/:userID/posts"
+        element={<PostFeed feedTitle="Posts" />}
+      />
+      <Route
+        key={`${location.pathname}-user-comments`}
+        path="/users/:userID/comments"
+        element={<UserCommentFeed />}
+      />
+      <Route
+        key={`${location.pathname}-user-favorites`}
         path="/users/:userID/favorites"
         element={<PostFeed feedTitle="Favorites" constraint="favorites" />}
       />
