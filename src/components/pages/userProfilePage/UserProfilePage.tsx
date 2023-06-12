1bad6d0c27aedc87d4decb1ab8952d3c22610cc9
mbright90 <michael.bright90@icloud.com>
2023-06-12 17:12:34 +0100

    Initiate and structure UserProfilePage

---
+++ src/components/pages/userProfilePage/UserProfilePage.tsx
@@ -0,0 +1,41 @@
+import Loading from '@components/loading/Loading'
+import UserProfileHeading from '@components/users/userProfileHeading/UserProfileHeading'
+import React, { useEffect, useState } from 'react'
+import useUsers from '@hooks/useUsers'
+import { useParams } from 'react-router-dom'
+import { UserType } from 'src/customTypes/types'
+
+import style from './UserProfilePage.module.scss'
+
+const UserProfilePage: React.FC = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(true)
+  const [user, setUser] = useState<UserType>({
+    uid: '',
+    displayName: '',
+    photoURL: '',
+    favorites: []
+  })
+
+  const { userID } = useParams()
+  const { loadUserProfile } = useUsers()
+
+  useEffect(() => {
+    const loadUser = async (): Promise<void> => {
+      const userData: UserType = await loadUserProfile(userID!)
+      setUser(userData)
+      if (userData) setIsLoading(false)
+    }
+
+    loadUser()
+  }, [])
+
+  if (isLoading) return <Loading />
+
+  return (
+    <div className={style.userProfilePageContainer}>
+      <UserProfileHeading user={user} />
+    </div>
+  )
+}
+
+export default UserProfilePage
