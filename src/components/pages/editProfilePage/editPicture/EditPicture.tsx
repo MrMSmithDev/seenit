f762623620bfbe5aaf67735f545215ee788e9803
mbright90 <michael.bright90@icloud.com>
2023-06-22 10:32:27 +0100

    Refactor method of initial image load

--- src/components/pages/editProfilePage/editPicture/EditPicture.tsx
+++ src/components/pages/editProfilePage/editPicture/EditPicture.tsx
@@ -1,19 +1,28 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import useAuth from '@hooks/useAuth'
 
 import style from './EditPicture.module.scss'
 
 interface EditPictureProps {
-  currentPicture: string
-  setTempPicture: (arg0: string | File) => void
+  setTempImage: (arg0: string | File) => void
 }
 
-const EditPicture: React.FC<EditPictureProps> = ({ currentPicture, setTempPicture }) => {
-  const [imagePreview, setImagePreview] = useState<string>(currentPicture)
+const EditPicture: React.FC<EditPictureProps> = ({ setTempImage }) => {
+  const [imagePreview, setImagePreview] = useState<string>('')
+
+  const { getUserImage, user } = useAuth()
+
+  useEffect(() => {
+    if (user) {
+      const userImage = getUserImage()
+      if (userImage) setImagePreview(userImage)
+    }
+  }, [user])
 
   const handleImageInput: React.ChangeEventHandler<HTMLInputElement> = (e) => {
     if (e.target.files) {
       const imageFile = e.target.files[0]
-      setTempPicture(imageFile)
+      setTempImage(imageFile)
       const preview = URL.createObjectURL(imageFile)
       setImagePreview(preview)
     }
