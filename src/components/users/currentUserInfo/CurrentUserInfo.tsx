6d3b3a1cd98e659cb3a452faeee4ff6da33de859
mbright90 <michael.bright90@icloud.com>
2023-06-24 12:00:38 +0100

    Add button styling and type safety

--- src/components/users/currentUserInfo/CurrentUserInfo.tsx
+++ src/components/users/currentUserInfo/CurrentUserInfo.tsx
@@ -17,7 +17,8 @@ const CurrentUserInfo: React.FC = () => {
   const [currentUser, setCurrentUser] = useState<UserType>({
     uid: '',
     displayName: '',
-    photoURL: ''
+    photoURL: '',
+    blurb: ''
   })
 
   useEffect(() => {
@@ -52,16 +53,7 @@ const CurrentUserInfo: React.FC = () => {
       <p className={style.favoriteCount}>
         <FontAwesomeIcon className={style.faIcon} icon={faStar} /> {usersFavoriteCount}
       </p>
-      <p className={style.userProfileBlurb}>
-        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat
-        mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper
-        suscipit, posuere a, pede. Donec nec justo eget felis facilisis fermentum. Aliquam porttitor
-        mauris sit amet orci. Aenean dignissim pellentesque felis. Morbi in sem quis dui placerat
-        ornare. Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras
-        consequat. Praesent dapibus, neque id cursus faucibus, tortor neque egestas auguae, eu
-        vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan
-        porttitor, facilisis luctus, metus.
-      </p>
+      <p className={style.userProfileBlurb}>{currentUser.blurb}</p>
       <Link className={style.editProfileButton} to="/edit-profile/">
         Edit Profile
       </Link>
