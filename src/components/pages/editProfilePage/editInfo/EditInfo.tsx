f615166e9681332983ec8cda2b75816ad9d0aa1f
mbright90 <michael.bright90@icloud.com>
2023-06-24 12:01:41 +0100

    Implement onChange events

--- src/components/pages/editProfilePage/editInfo/EditInfo.tsx
+++ src/components/pages/editProfilePage/editInfo/EditInfo.tsx
@@ -1,23 +1,41 @@
 import React from 'react'
+import { UserType } from 'src/customTypes/types'
 
 import style from './EditInfo.module.scss'
 
 interface EditInfoProps {
+  currentUser: UserType
   setTempDisplayName: (arg0: string) => void
   setTempProfileText: (arg0: string) => void
 }
 
-const EditInfo: React.FC<EditInfoProps> = ({ setTempDisplayName, setTempProfileText }) => {
+const EditInfo: React.FC<EditInfoProps> = ({
+  currentUser,
+  setTempDisplayName,
+  setTempProfileText
+}) => {
+  const handleDisplayNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTempDisplayName(e.target.value)
+  }
+
+  const handleProfileTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTempProfileText(e.target.value)
+  }
+
   return (
     <div className={style.editInfoContainer}>
       <form className={style.infoForm}>
         <div className={style.editInfoInputContainer}>
-          <label>label</label>
-          <input />
+          <label>Display Name:</label>
+          <input
+            type="text"
+            defaultValue={currentUser.displayName}
+            onChange={handleDisplayNameChange}
+          />
         </div>
         <div className={style.editInfoInputContainer}>
-          <label>Label</label>
-          <textarea />
+          <label>Profile Blurb</label>
+          <textarea defaultValue={currentUser.blurb} onChange={handleProfileTextChange} />
         </div>
       </form>
     </div>
