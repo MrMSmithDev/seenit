29c9f839b3b11c1c93e5179e37c3e7ecb1d4d645
mbright90 <michael.bright90@icloud.com>
2023-06-25 14:01:06 +0100

    Add low rated filters

--- src/utils/filters.js
+++ src/utils/filters.js
@@ -1,7 +1,8 @@
 const filters = [
   { text: 'Newest', id: 'new' },
   { text: 'Oldest', id: 'oldest' },
-  { text: 'Top Rated', id: 'top' }
+  { text: 'Top Rated', id: 'top' },
+  { text: 'Low Rated', id: 'lowest' }
 ]
 
 export default filters
