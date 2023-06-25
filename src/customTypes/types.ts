d41a143a6a764aa7b50146bba765e9830e633382
mbright90 <michael.bright90@icloud.com>
2023-06-25 14:05:19 +0100

    Slimline imports

--- src/customTypes/types.ts
+++ src/customTypes/types.ts
@@ -1,5 +1,4 @@
-import { Timestamp } from 'firebase/firestore'
-import { OrderByDirection } from 'firebase/firestore'
+import { OrderByDirection, Timestamp } from 'firebase/firestore'
 
 export interface PostType {
   ID: string
