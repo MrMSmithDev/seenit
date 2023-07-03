0da8cef4e67ab975315193ea484c726a52f16d53
mbright90 <michael.bright90@icloud.com>
2023-07-03 17:14:30 +0100

    Add useEffect to clear modals

--- src/hooks/useModal/useModal.ts
+++ src/hooks/useModal/useModal.ts
@@ -1,7 +1,32 @@
-import React, { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 function useModal() {
+  const [isShowing, setIsShowing] = useState<boolean>(false)
+  const [currentMessage, setCurrentMessage] = useState<string>('')
 
+  useEffect(() => {
+    let modalTimeout: NodeJS.Timeout
+    if (isShowing) {
+      modalTimeout = setTimeout(() => {
+        setIsShowing(false)
+      }, 3000)
+    }
+
+    return () => {
+      if (modalTimeout) clearTimeout(modalTimeout)
+    }
+  }, [isShowing])
+
+  function toggle(message = '') {
+    setCurrentMessage(message)
+    setIsShowing(!isShowing)
+  }
+
+  return {
+    isShowing,
+    currentMessage,
+    toggle
+  }
 }
 
 export default useModal
