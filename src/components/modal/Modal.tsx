f70e88a8e1de88c22e39e5ba0cb79120f07b4a70
mbright90 <michael.bright90@icloud.com>
2023-07-03 17:15:07 +0100

    Initiate and implement modal component

---
+++ src/components/modal/Modal.tsx
@@ -0,0 +1,30 @@
+/* eslint-disable prettier/prettier */
+// Disabled due to conflicting errors with spacing within createPortal
+import { faCircleXmark } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from 'react'
+import { createPortal } from 'react-dom'
+
+import style from './Modal.module.scss'
+
+interface ModalProps {
+  message: string
+  isShowing: boolean
+  toggle: () => void
+}
+
+const Modal: React.FC<ModalProps> = ({ message, isShowing, toggle }) => {
+  return isShowing
+    ? createPortal(
+      <div className={style.modalOverlay}>
+        <div className={style.modalContainer}>
+          <p>{message}</p>
+          <button onClick={toggle}><FontAwesomeIcon icon={faCircleXmark}/></button>
+        </div>
+      </div>,
+      document.body
+    )
+    : null
+}
+
+export default Modal
