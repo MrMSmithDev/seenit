929a6f9600213dd169cb97446c484e6555d91fb2
mbright90 <michael.bright90@icloud.com>
2023-07-04 17:01:15 +0100

    Add className to button

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -19,7 +19,7 @@ const Modal: React.FC<ModalProps> = ({ message, isShowing, toggle }) => {
       <div className={style.modalOverlay}>
         <div className={style.modalContainer}>
           <p>{message}</p>
-          <button onClick={toggle}><FontAwesomeIcon icon={faCircleXmark}/></button>
+          <button className={style.closeModal} onClick={toggle}><FontAwesomeIcon icon={faCircleXmark}/></button>
         </div>
       </div>,
       document.body
