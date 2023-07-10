a993a8b1f0b97b00f7fc906fb0bbdd04874369fc
mbright90 <michael.bright90@icloud.com>
2023-07-10 16:51:37 +0100

    Import and render UserBarWide

--- src/components/userBar/UserBar.tsx
+++ src/components/userBar/UserBar.tsx
@@ -1,56 +1,56 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import useAuth from '@hooks/useAuth'
 
-import style from './UserBar.module.scss'
+import UserBarWide from '@components/userBar/userBarWide'
 
 const UserBar: React.FC = () => {
   const { user } = useAuth()
 
   return (
-    <div className={style.userBar}>
-      <Link to="/" className={style.sidebarLink} tabIndex={1} data-testid="sidebarLink">
-        Home
-      </Link>
-      <Link to="/posts/new" className={style.sidebarLink} tabIndex={2} data-testid="sidebarLink">
-        New Post
-      </Link>
-      <Link
-        to={`/users/${user?.uid}/posts`}
-        className={style.sidebarLink}
-        tabIndex={3}
-        data-testid="sidebarLink"
-      >
-        My Posts
-      </Link>
-      <Link
-        to={`/users/${user?.uid}/comments`}
-        className={style.sidebarLink}
-        tabIndex={4}
-        data-testid="sidebarLink"
-      >
-        My Comments
-      </Link>
-      <Link
-        to={`/users/${user?.uid}/favorites`}
-        className={style.sidebarLink}
-        tabIndex={5}
-        data-testid="sidebarLink"
-      >
-        My Favorites
-      </Link>
-      <Link
-        to={`/users/profile/${user?.uid}`}
-        className={style.sidebarLink}
-        tabIndex={6}
-        data-testid="sidebarLink"
-      >
-        My Profile
-      </Link>
-      <Link to={'/settings'} className={style.sidebarLink} tabIndex={7} data-testid="sidebarLink">
-        Settings
-      </Link>
-    </div>
+    <UserBarWide user={user!} />
+    // <div className={style.userBarLarge}>
+    //   <Link to="/" className={style.sidebarLink} tabIndex={1} data-testid="sidebarLink">
+    //     Home
+    //   </Link>
+    //   <Link to="/posts/new" className={style.sidebarLink} tabIndex={2} data-testid="sidebarLink">
+    //     New Post
+    //   </Link>
+    //   <Link
+    //     to={`/users/${user?.uid}/posts`}
+    //     className={style.sidebarLink}
+    //     tabIndex={3}
+    //     data-testid="sidebarLink"
+    //   >
+    //     My Posts
+    //   </Link>
+    //   <Link
+    //     to={`/users/${user?.uid}/comments`}
+    //     className={style.sidebarLink}
+    //     tabIndex={4}
+    //     data-testid="sidebarLink"
+    //   >
+    //     My Comments
+    //   </Link>
+    //   <Link
+    //     to={`/users/${user?.uid}/favorites`}
+    //     className={style.sidebarLink}
+    //     tabIndex={5}
+    //     data-testid="sidebarLink"
+    //   >
+    //     My Favorites
+    //   </Link>
+    //   <Link
+    //     to={`/users/profile/${user?.uid}`}
+    //     className={style.sidebarLink}
+    //     tabIndex={6}
+    //     data-testid="sidebarLink"
+    //   >
+    //     My Profile
+    //   </Link>
+    //   <Link to={'/settings'} className={style.sidebarLink} tabIndex={7} data-testid="sidebarLink">
+    //     Settings
+    //   </Link>
+    // </div>
   )
 }
 
