5a3cb677a4924c5eb51d926cbb061ad021d66214
mbright90 <michael.bright90@icloud.com>
2023-07-14 10:16:06 +0100

    Initiate PostControls component directory

---
+++ src/components/posting/postControls/PostControls.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import style from './PostControls.module.scss'
+
+interface PostControlsProps {
+  postID: string
+}
+
+const PostControls: React.FC<PostControlsProps> = ({ postID }) => {
+  return <div></div>
+}
+
+export default PostControls
