e2cba60f57ddf4dafda0e62fbd0d3e16b362be5d
mbright90 <michael.bright90@icloud.com>
2023-07-18 11:28:46 +0100

    Implement emptyUser util placeholder

---
+++ src/utils/placeholders/emptyUser/emptyUser.ts
@@ -0,0 +1,12 @@
+import { UserType } from 'src/customTypes/types'
+
+const emptyUser: UserType = {
+  uid: '',
+  displayName: 'Not Found',
+  photoURL: '',
+  blurb: '',
+  comments: 0,
+  posts: 0
+}
+
+export default emptyUser
