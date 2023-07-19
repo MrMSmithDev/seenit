bc690de4f1827a74095deeab2e1e26c425bf4ec2
mbright90 <michael.bright90@icloud.com>
2023-07-19 10:34:20 +0100

    Add logic for displaying comment editing

--- src/components/comments/comment/Comment.tsx
+++ src/components/comments/comment/Comment.tsx
@@ -16,6 +16,7 @@ interface CommentProps {
 
 const Comment: React.FC<CommentProps> = ({ comment }) => {
   const [author, setAuthor] = useState<UserType>(emptyUser)
+  const [editing, setEditing] = useState<boolean>(false)
   const { user } = useAuth()
   const { loadUserProfile } = useUsers()
 
@@ -34,15 +35,28 @@ const Comment: React.FC<CommentProps> = ({ comment }) => {
   let commentControlsElement: React.ReactNode | null
   if (user) {
     commentControlsElement =
-      user.uid === author.uid ? <CommentControls commentID={comment.ID} /> : null
+      user.uid === author.uid ? (
+        <CommentControls commentID={comment.ID} editState={editing} setEditState={setEditing} />
+      ) : null
   }
+
+  let commentState: React.ReactNode
+
+  if (editing)
+    commentState = (
+      <div className={style.editContainer}>
+        <textarea className={style.editCommentInput} value={comment.body} />
+        <button className={style.submitCommentEdit}>Publish edit</button>
+      </div>
+    )
+  else commentState = <p className={style.commentBody}>{comment.body}</p>
 
   return (
     <div className={style.commentContainer} data-comment-post-id={comment.postID}>
       <VoteContainer comment={comment} />
       <div className={style.comment}>
         <div className={style.commentTimestamp}>{formattedTime}</div>
-        <p className={style.commentBody}>{comment.body}</p>
+        {commentState}
         <div className={style.commentInfo}>
           {commentControlsElement}
           <AuthorInfo author={author} link={true} />
