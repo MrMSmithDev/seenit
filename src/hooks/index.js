ced3eee4776bbb2a9ba7429dd1152de08a9f8816
mbright90 <michael.bright90@icloud.com>
2023-07-27 08:40:01 +0100

    Export useInfiniteScroll hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,7 +1,8 @@
 import useAuth from './useAuth'
 import useComments from './useComments'
+import useInfiniteScroll from './useInfiniteScroll'
 import useNotification from './useNotification'
 import usePosts from './usePosts'
 import useUsers from './useUsers'
 
-export { useAuth, useComments, useNotification, usePosts, useUsers }
+export { useAuth, useComments, useInfiniteScroll, useNotification, usePosts, useUsers }
