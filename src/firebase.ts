4213ba1e1eab7ee2dba4841851a6f7dff7ec686f
mbright90 <michael.bright90@icloud.com>
2023-08-06 19:21:20 +0100

    Convert js to ts

---
+++ src/firebase.ts
@@ -0,0 +1,18 @@
+/* eslint-disable no-console */
+import { FirebaseStorage, getStorage } from '@firebase/storage'
+import { initializeApp, FirebaseApp } from 'firebase/app'
+import { Auth, getAuth } from 'firebase/auth'
+import { Firestore, getFirestore } from 'firebase/firestore'
+import { firebaseConfig } from './firebaseConfig'
+
+let app: FirebaseApp
+
+try {
+  app = initializeApp(firebaseConfig)
+} catch (error) {
+  console.error('Error loading firebase:', error)
+}
+
+export const auth: Auth = getAuth(app!)
+export const firestore: Firestore = getFirestore(app!)
+export const storage: FirebaseStorage = getStorage(app!)
