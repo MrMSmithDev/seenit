30900f426cee7110e3ccb52a2f30322ddb6d963c
mbright90 <michael.bright90@icloud.com>
2023-08-09 10:30:04 +0100

    Alter userCommentFeed for inf scroll

--- src/components/comments/userCommentFeed/UserCommentFeed.tsx
+++ src/components/comments/userCommentFeed/UserCommentFeed.tsx
@@ -1,7 +1,8 @@
 import Loading from '@components/loading'
 import PostPreview from '@components/posting/postPreview'
-import { useComments, usePosts, useUsers } from '@hooks/index'
-import React, { ReactNode, useEffect, useState } from 'react'
+import { usePosts, useUsers } from '@hooks/index'
+import useInfiniteScrollComments from '@hooks/useInfiniteScrollComments/useInfiniteScrollComments'
+import React, { ReactNode, useEffect, useRef, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { CommentType } from 'src/customTypes/types'
 import Comment from '../comment'
@@ -23,11 +24,18 @@ const UserCommentFeed: React.FC = () => {
 
   const [isLoading, setIsLoading] = useState<boolean>(true)
   const [userDisplayName, setUserDisplayName] = useState<string>('')
-  const [feedData, setFeedData] = useState<ReactNode[]>([])
+  const [feedData, setFeedDataState] = useState<ReactNode[]>([])
+
+  const feedDataRef = useRef<ReactNode[]>(feedData)
+  const setFeedData = (newData: ReactNode[]): void => {
+    feedDataRef.current = newData
+    setFeedDataState(newData)
+  }
 
   const { getUsersDisplayName } = useUsers()
-  const { loadUsersComments } = useComments()
   const { loadCurrentPost } = usePosts()
+
+  const { comments, loadCommentScroll } = useInfiniteScrollComments()
 
   useEffect(() => {
     const getUsersName = async (): Promise<void> => {
@@ -39,11 +47,23 @@ const UserCommentFeed: React.FC = () => {
   }, [])
 
   useEffect(() => {
-    const retrieveComments = async (): Promise<void> => {
-      const retrievedComments: CommentType[] = await loadUsersComments(userID!)
+    const setComments = async (): Promise<void> => {
+      const fetchComments = async (): Promise<void> => {
+        await loadCommentScroll(userID!)
+      }
+
+      await fetchComments()
+    }
+
+    setComments()
+  }, [userID])
+
+  useEffect(() => {
+    const setPostsForComments = async (): Promise<void> => {
       const postArr: PostIDsWithComments[] = []
 
-      retrievedComments.forEach((currentComment) => {
+      // Set array of comment IDs from author for each post
+      comments.forEach((currentComment: CommentType) => {
         const postID = currentComment.postID
         if (checkPostID(postArr, postID)) {
           const arrIndex = postArr.findIndex((post) => post.postID === currentComment.postID)
@@ -58,6 +78,7 @@ const UserCommentFeed: React.FC = () => {
         post.commentArr.sort((a, b) => (a.timeStamp.toDate() < b.timeStamp.toDate() ? -1 : 1))
       })
 
+      // Create Array of nodes
       const feedArr = await Promise.all(
         postArr.map(async (postWithCommentsObj) => {
           const { postID, commentArr } = postWithCommentsObj
@@ -71,19 +92,60 @@ const UserCommentFeed: React.FC = () => {
           )
         })
       )
+
       setFeedData(feedArr)
       setIsLoading(false)
     }
 
-    retrieveComments()
-  }, [])
+    setPostsForComments()
+  }, [comments])
+
+  // useEffect(() => {
+  //   const retrieveComments = async (): Promise<void> => {
+  //     const retrievedComments: CommentType[] = await loadUsersComments(userID!)
+  //     const postArr: PostIDsWithComments[] = []
+
+  //     retrievedComments.forEach((currentComment) => {
+  //       const postID = currentComment.postID
+  //       if (checkPostID(postArr, postID)) {
+  //         const arrIndex = postArr.findIndex((post) => post.postID === currentComment.postID)
+  //         postArr[arrIndex].commentArr.push(currentComment)
+  //       } else {
+  //         postArr[postArr.length] = { postID, commentArr: [currentComment] }
+  //       }
+  //     })
+
+  //     // Sort comments by date for each post:
+  //     postArr.forEach((post) => {
+  //       post.commentArr.sort((a, b) => (a.timeStamp.toDate() < b.timeStamp.toDate() ? -1 : 1))
+  //     })
+
+  //     const feedArr = await Promise.all(
+  //       postArr.map(async (postWithCommentsObj) => {
+  //         const { postID, commentArr } = postWithCommentsObj
+  //         const currentPost = await loadCurrentPost(postID)
+  //         return (
+  //           <div className={style.postWithComments} key={postID}>
+  //             <PostPreview currentPost={currentPost} />
+  //             <Comment comment={commentArr[0]} />
+  //             {commentArr.length > 1 ? <FurtherComments commentArr={commentArr.slice(1)} /> : null}
+  //           </div>
+  //         )
+  //       })
+  //     )
+  //     setFeedData(feedArr)
+  //     setIsLoading(false)
+  //   }
+
+  //   retrieveComments()
+  // }, [])
 
   if (isLoading) return <Loading />
 
   return (
     <div className={style.userCommentFeed}>
       <p className={style.feedTitle}>{`${userDisplayName}'s Comments`}</p>
-      <div className={style.feedContainer}>{feedData}</div>
+      <div className={style.feedContainer}>{feedDataRef.current}</div>
     </div>
   )
 }
