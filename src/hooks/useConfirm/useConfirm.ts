7638c98755b7e00d9fb8057a6d67482cbd74ddbd
mbright90 <michael.bright90@icloud.com>
2023-08-20 10:55:59 +0100

    Adapt method of creating callback function

--- src/hooks/useConfirm/useConfirm.ts
+++ src/hooks/useConfirm/useConfirm.ts
@@ -3,19 +3,16 @@ import { useState } from 'react'
 function useConfirm() {
   const [isShowing, setIsShowing] = useState<boolean>(false)
   const [message, setMessage] = useState<string>('')
-  const [callbackFunction, setCallbackFunction] = useState<(() => Promise<void>) | null>(null)
 
-  function toggle(newMessage = '', newCallbackFunction: (() => Promise<void>) | null = null): void {
+  function toggle(newMessage = ''): void {
     setMessage(newMessage)
-    setCallbackFunction(newCallbackFunction)
     setIsShowing(!isShowing)
   }
 
   return {
     isShowing,
     message,
-    toggle,
-    callbackFunction
+    toggle
   }
 }
 
