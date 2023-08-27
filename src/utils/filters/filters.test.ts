45805920ece645ebeff59aa2d7d16080993320ee
mbright90 <michael.bright90@icloud.com>
2023-08-27 10:31:13 +0100

    Refine tests

--- src/utils/filters/filters.test.ts
+++ src/utils/filters/filters.test.ts
@@ -4,28 +4,22 @@ import { filterSwitch } from './filters'
 
 describe('filterSwitch', () => {
   it('should return a FilterQuery object of attribute: timeStamp, order: desc when an invalid string is passed in', () => {
-    // Act
     const result: FilterQuery = filterSwitch('not accepted')
 
-    // Assert
     expect(result.attribute).toBe('timeStamp')
     expect(result.order).toBe('desc')
   })
 
   it('should return a FilterQuery object of attribute: favorites, order: asc when passed low rated', () => {
-    // Act
     const result: FilterQuery = filterSwitch('low rated')
 
-    // Assert
     expect(result.attribute).toBe('favorites')
     expect(result.order).toBe('asc')
   })
 
   it('should return correct FilterQuery object regardless of letter case', () => {
-    // Act
     const result: FilterQuery = filterSwitch('OlDESt')
 
-    // Assert
     expect(result.attribute).toBe('timeStamp')
     expect(result.order).toBe('asc')
   })
