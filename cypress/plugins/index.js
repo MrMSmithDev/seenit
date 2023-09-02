eb5ef8550f35ade8de3ed8c03e2b8f0462bad954
mbright90 <michael.bright90@icloud.com>
2023-09-02 17:14:19 +0100

    Config cypress-firebase plugin

---
+++ cypress/plugins/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+const admin = require('firebase-admin')
+const cypressFirebasePlugin = require('cypress-firebase').plugin
+
+module.exports = (on, config) => {
+  const extendedConfig = cypressFirebasePlugin(on, config, admin)
+
+  return extendedConfig
+}
