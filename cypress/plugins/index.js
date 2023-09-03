86d4b7d7a84e2e51c3e522e7bab5214ffe1f08f9
mbright90 <michael.bright90@icloud.com>
2023-09-03 17:08:22 +0100

    Remove unnecessary config

--- cypress/plugins/index.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-const admin = require('firebase-admin')
-const cypressFirebasePlugin = require('cypress-firebase').plugin
-
-module.exports = (on, config) => {
-  const extendedConfig = cypressFirebasePlugin(on, config, admin)
-
-  return extendedConfig
-}
