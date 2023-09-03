00ad5a8a8550f911e25ad1fedf9fda423415f2f0
mbright90 <michael.bright90@icloud.com>
2023-09-03 17:07:21 +0100

    Add extendedConfig

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'cypress'
+import { plugin as cypressFirebasePlugin } from 'cypress-firebase'
+import admin from 'firebase-admin'
+
+import serviceAccount from './serviceAccount.json'
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      cypressFirebasePlugin(on, config, admin, {
+        credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
+      })
     }
   }
 })
