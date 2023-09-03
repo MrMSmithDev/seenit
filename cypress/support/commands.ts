c1a7f7a25798b27eaf49f82e409977e159e8fbb2
mbright90 <michael.bright90@icloud.com>
2023-09-03 17:07:57 +0100

    Alter app initialisation

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,3 +1,4 @@
+import { firebaseConfig } from '@src/firebaseConfig'
 import { attachCustomCommands } from 'cypress-firebase'
 import firebase from 'firebase/compat/app'
 
@@ -5,13 +6,6 @@ import 'firebase/compat/auth'
 import 'firebase/compat/database'
 import 'firebase/compat/firestore'
 
-firebase.initializeApp({
-  apiKey: 'AIzaSyC0HUz0CDc2mo3kLF60VRKzSeRqpSY9ggo',
-  authDomain: 'seenit-9e81d.firebaseapp.com',
-  projectId: 'seenit-9e81d',
-  storageBucket: 'seenit-9e81d.appspot.com',
-  messagingSenderId: '197060165111',
-  appId: '1:197060165111:web:90182ba92872e64b01c2f2'
-})
+firebase.initializeApp(firebaseConfig)
 
 attachCustomCommands({ Cypress, cy, firebase })
