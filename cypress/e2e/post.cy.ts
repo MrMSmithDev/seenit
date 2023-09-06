100b0179d3914dfbacf197ec5a59adb73fb5b86f
mbright90 <michael.bright90@icloud.com>
2023-09-06 10:29:13 +0100

    Add link testing

--- cypress/e2e/post.cy.ts
+++ cypress/e2e/post.cy.ts
@@ -1,25 +1,54 @@
+function visitPost() {
+  cy.visit('http://localhost:8080')
+  cy.get('[data-testid="post-preview"]').first().click()
+}
+
 describe('Visits a post link', () => {
   beforeEach(() => {
     cy.login()
-    cy.visit('http://localhost:8080')
   })
 
   it('Shows a post with a valid structure when given a valid address', () => {
-    
+    // Act
+    visitPost()
+
+    // Assert valid post structure
+    cy.get('[data-testid="post-title"]').should('exist')
+    cy.get('[data-testid="post-body"]').should('exist')
+    cy.get('[data-testid="post-info"]').should('exist')
+    cy.get('[data-testid="author-info-link"]').should('exist')
   })
 
   it('Visits a post page when a valid link is clicked from "Home"', () => {
     // Act
-    cy.visit('http://localhost:8080/')
-    cy.get('[data-testid="post-preview"]').first().click()
+    visitPost()
 
-    // Assert
+    // Assert Post Exists
     cy.get('[data-testid="post"]').should('exist')
   })
 
   it('Visits a post page when a valid link is clicked from "My Posts"', () => {
     // Act
     cy.visit('http://localhost:8080/')
+    cy.contains('[data-testid="navbar-links"]:visible > a', 'My Posts').click({
+      scrollBehavior: false
+    })
+    cy.url().should('include', '/users/')
+    cy.url().should('include', '/posts')
+    cy.get('[data-testid="post-preview"]').first().click()
+
+    // Assert
+    cy.get('[data-testid="post"]').should('exist')
+  })
+
+  it('Visits a post page when a valid link is clicked from "My favorites"', () => {
+    // Act
+    cy.visit('http://localhost:8080/')
+    cy.contains('[data-testid="navbar-links"]:visible > a', 'My Favorites').click({
+      scrollBehavior: false
+    })
+    cy.url().should('include', '/users/')
+    cy.url().should('include', '/favorites')
     cy.get('[data-testid="post-preview"]').first().click()
 
     // Assert
