a43b171fe43a47fc9eed7d4ae09d0311b34c5cc4
mbright90 <michael.bright90@icloud.com>
2023-09-14 10:32:38 +0100

    Add comment form test descriptions

--- cypress/e2e/comment.cy.ts
+++ cypress/e2e/comment.cy.ts
@@ -11,6 +11,7 @@ describe('Displays comments correctly', () => {
     // Assert
     const comment = cy.get('[data-testid="comment"]')
 
+    comment.should('exist')
     comment.get('[data-testid="author-info-link"]').should('exist')
     comment.get('[data-testid="comment-score-container"]').should('exist')
     comment.get('[data-testid="comment-body"]').should('exist')
@@ -24,6 +25,26 @@ describe('Displays comments correctly', () => {
     // Assert
     cy.contains('No comments have been made').should('exist')
   })
+
+  it('Shows a new comment form on all posts', () => {
+    cy.visit('http://localhost:8080')
+
+    cy.get('data-testid="comment-count"').first().should('exist').click()
+
+    // Assert
+    const commentForm = cy.get('[data-testid="new-comment-form"]')
+    commentForm.should('exist')
+    commentForm.get('new-comment-input').should('exist')
+    commentForm.get('new-comment-publish-btn')
+  })
+})
+
+describe('handles new comment form behavior correctly', () => {
+  it('allows input in the new comment form', () => {})
+
+  it('shows an active publish button when commentBody.length is greater than zero', () => {})
+
+  it('shows a disabled publish button when commentBody.length is less than zero', () => {})
 })
 
 export {}
