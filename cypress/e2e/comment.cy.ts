b50689d4b4566e24b72c8a749ec426343840181a
mbright90 <michael.bright90@icloud.com>
2023-09-15 09:10:59 +0100

    Add button disability tests

--- cypress/e2e/comment.cy.ts
+++ cypress/e2e/comment.cy.ts
@@ -6,7 +6,7 @@ describe('Displays comments correctly', () => {
   it('correctly displays comments on posts which include at least one comment', () => {
     cy.visit('http://localhost:8080')
 
-    cy.get('[data-testid="comment-count"]').contains('1').first().should('exist').click()
+    cy.get('[data-testid="comment-count"]').contains('1').first().should('be.visible').click()
 
     // Assert
     const comment = cy.get('[data-testid="comment"]')
@@ -20,7 +20,7 @@ describe('Displays comments correctly', () => {
   it('displays a no comments message when no comments are made', () => {
     cy.visit('http://localhost:8080')
 
-    cy.get('[data-testid="comment-count"]').contains('0').first().should('exist').click()
+    cy.get('[data-testid="comment-count"]').contains('0').first().should('be.visible').click()
 
     // Assert
     cy.contains('No comments have been made').should('exist')
@@ -29,7 +29,7 @@ describe('Displays comments correctly', () => {
   it('Shows a new comment form on all posts', () => {
     cy.visit('http://localhost:8080')
 
-    cy.get('data-testid="comment-count"').first().should('exist').click()
+    cy.get('data-testid="post-preview"').first().should('be.visible').click()
 
     // Assert
     const commentForm = cy.get('[data-testid="new-comment-form"]')
@@ -40,11 +40,36 @@ describe('Displays comments correctly', () => {
 })
 
 describe('handles new comment form behavior correctly', () => {
-  it('allows input in the new comment form', () => {})
+  it('allows input in the new comment form', () => {
+    cy.visit('http://localhost:8080')
 
-  it('shows an active publish button when commentBody.length is greater than zero', () => {})
+    cy.get('data-testid="post-preview"').first().should('be.visible').click()
+    const commentInput = cy.get('data-testid="new-comment-input"')
+    commentInput.should('exist').type('Comment test')
 
-  it('shows a disabled publish button when commentBody.length is less than zero', () => {})
+    // Assert
+    commentInput.should('have.value', 'Comment test')
+  })
+
+  it('shows an active publish button when commentBody.length is greater than zero', () => {
+    cy.visit('http://localhost:8080')
+
+    cy.get('data-testid="post-preview"').first().should('be.visible').click()
+    cy.get('data-testid="comment-form-input"').should('exist').type('Activate publish button')
+
+    // Assert
+    cy.get('new-comment-publish-btn').should('not.be.disabled')
+  })
+
+  it('shows a disabled publish button when commentBody.length is less than zero', () => {
+    cy.visit('http://localhost:8080')
+
+    cy.get('data-testid="post-preview"').first().should('be.visible').click()
+    cy.get('data-testid="comment-form-input"').should('have.value', '')
+
+    // Assert
+    cy.get('new-comment-publish-btn').should('be.disabled')
+  })
 })
 
 export {}
