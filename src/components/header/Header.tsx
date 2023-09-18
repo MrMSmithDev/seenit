4cd7cbb70502464f94bf3c7feb8c030ff7e1c36a
mbright90 <michael.bright90@icloud.com>
2023-09-18 12:01:16 +0100

    Linting fix

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,8 +6,6 @@ import UserStatus from './userStatus'
 import style from './Header.module.scss'
 
 const Header: React.FC = () => {
-  //Create useState here which adjusts the size of the header when scrolled down.
-
   return (
     <header className={style.header} data-testid="header">
       <img className={style.headerLogo} src={logo} alt="SeenIt-logo-blue"></img>
