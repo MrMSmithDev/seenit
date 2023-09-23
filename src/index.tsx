85f7825f6b7739f10a0eb2945aa2b3ff9d898d81
mbright90 <michael.bright90@icloud.com>
2023-09-23 10:11:37 +0100

    Clean up index.tsx comments

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,8 @@
-/* eslint-disable @typescript-eslint/no-unused-vars, no-console */
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from '@components/app'
-import { initializeApp } from 'firebase/app'
-
-import { firebaseConfig } from './firebaseConfig'
 
 import '@styles/styles.scss'
-
-// try {
-//   initializeApp(firebaseConfig)
-// } catch (error) {
-//   console.error('Could not initialize Firestore', error)
-// }
 
 const root = createRoot(document.getElementById('root')!)
 
